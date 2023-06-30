24f4356f2ceaef5167cbef22b4fa1aad5d8cf939
HP <HP@DESKTOP-LVL019S>
2023-06-30 11:31:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        { 
+            Console.WriteLine("Hello, World");
+            Console.WriteLine("Nazywan się Edyta Nowakowska");
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
